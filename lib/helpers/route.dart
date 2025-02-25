431c35f8b0559d0094ddd3ddbe8ae448d8cba95e
Zimmer550i <sakif049@gmail.com>
2025-02-25 17:30:32 +0600

    PROJECT SETUP

---
+++ lib/helpers/route.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AppRoutes {
+  static String splashScreen = "/splash_screen";
+
+  static Map<String, Widget> routeWidgets  = {
+    
+  };
+
+  static List<GetPage> pages = [
+    ...routeWidgets.entries.map(
+      (e) => GetPage(name: e.key, page: () => e.value),
+    ),
+  ];
+}
---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+ThemeData light({Color color = const Color(0xFF039D55)}) => ThemeData();
